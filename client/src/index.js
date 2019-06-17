05ad96db9fe4ef1a7c113f867d8eb59d65fcfc1f
Amanda Garcia <algarcian@outlook.com>
2019-06-17 12:37:47 -0600

    Added devise axios

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,10 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./providers/AuthProvider";
 import "semantic-ui-css/semantic.min.css";
+import {initMiddleware} from 'devise-axios'; 
+
+// will take axios requests and will look for any sort of authorization header - what your token will be saved as in local storage. 
+initMiddleware();
 
 ReactDOM.render(
   <AuthProvider>
