89f207b87703711c738cdde8442e4a2559a6f59e
Amanda Garcia <algarcian@outlook.com>
2019-06-17 11:13:11 -0600

    Added react router and authprovider to index

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./providers/AuthProvider";
+import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
+  document.getElementById("root")
+);
